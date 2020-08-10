6cc98079056d40167257c1f99bb8be1f7afe63b7
Matthew Lal <matteodlal@gmail.com>
2020-08-10 04:15:14 +0000

    ran npm audit fix and added builder

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: uncomment next line and enter your own public API key
-  // builderAPIKey: '<YOUR PUBLIC API KEU>'
+  builderAPIKey: 'e6b8ce5842eb449fb37c66c14276913d'
 };
