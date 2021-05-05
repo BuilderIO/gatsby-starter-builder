8dd390a2b7d7f4b36a5557280ca93f1edcfc6a67
Dylan C. Kendal <dylan@builder.io>
2021-05-05 11:50:07 -0400

    Update to gatsby v3 (#23)
    * style: format the readme
    
    * feat: source builder api key from env
    
    * chore: bump package lock
    
    Just the result of npm install on npm6
    
    * fix: npm audit fix
    
    * chore: bump browserslist

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // using module.exports here so gatsby-config can read it
 module.exports = {
   // TODO: uncomment next line and enter your own public API key
-  // builderAPIKey: '<YOUR PUBLIC API KEU>'
+  builderAPIKey: process.env.BUILDER_API_KEY
 };
